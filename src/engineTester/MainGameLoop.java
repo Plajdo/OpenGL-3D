8bc42e291a7416054f07a829bf7464bcd3e75e89
Plajdo <31418257+Plajdo@users.noreply.github.com>
2017-12-17 09:16:44 +0100

    Shaders

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -15,6 +16,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] vertices = {
 				-0.5f, 0.5f, 0f,
@@ -33,11 +35,14 @@ public class MainGameLoop {
 		while(!Display.isCloseRequested()){
 			
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 			
 		}
 		
+		shader.cleanup();
 		loader.cleanup();
 		DisplayManager.closeDisplay();
 
