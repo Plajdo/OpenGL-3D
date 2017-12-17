ddb5db582ae95a7cfa14bbab314486c45fbf90d3
Plajdo <31418257+Plajdo@users.noreply.github.com>
2017-12-17 08:40:59 +0100

    Index Buffers

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -18,14 +18,17 @@ public class MainGameLoop {
 		
 		float[] vertices = {
 				-0.5f, 0.5f, 0f,
-				-0.5f -0.5f, 0f,
+				-0.5f, -0.5f, 0f,
 				0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f
+				0.5f, 0.5f, 0f
+ 		};
+		
+		int[] indices = {
+				0, 1, 3,
+				3, 1, 2
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		while(!Display.isCloseRequested()){
 			
